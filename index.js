43223185b5ff2372206ce3d716dafdc77619a042
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-05-26 08:41:10 -0500

    commit tree 9e6b15ff05006368a827482e361b12125c32a574

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+const employee = {
+    name:"Value1",
+    streetAddress:"Value2"    
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return{...employee,[key]:value,
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key]=value;
+        return employee;  
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key){
+        return[...employee[key]];
+}
